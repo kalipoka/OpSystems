3bc2e65a4db8416f7ed0328f6bb302995ea8c0d3
Alex S <saraevalex@gmail.com>
2017-12-08 18:02:37 +0200

    Added main

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -4,7 +4,6 @@
 #include "Threads.h"
 #include "WriteToLog.h"
 
-//*****************564zfdhfgjjklzdlgnkDLnk
 using namespace std;
 
 WriteToLog logF;
@@ -15,37 +14,42 @@ int main (int argc, char **argv){
     // we then need to create the threads and initialize all the ATMS with the needed data
 
 
-
-
     // This is the version without concurrency
+/*
     BankDB Citi;// = BankDB();
     ATM tryATM(1, &Citi);
-    tryATM.ReadCommandsFromFile("D:\\semesters\\Winter_2018\\046209\\HW2_withGIT\\OpSystems\\hw2\\in1.txt");
+    tryATM.ReadCommandsFromFile("../in1.txt");
     Citi.PrintAccounts();
     Citi.ChargeFee();
+    Citi.PrintAccounts();
+    Citi.ChargeFee();
+    Citi.PrintAccounts();
+    */
 
-/*
+    // Locking and threads version
+
+
        BankDB Citi;
 
        thread_data_atm atm_args;
 
        // initialize arguments
-       string path ="C:\\Users\\alexey\\Source\\Repos\\OpSystems\\hw2\\in1.txt";
-
+       string path ="../in1.txt";
        atm_args.atm_id = 1;
        atm_args.pathToInstructions = path;
        atm_args.DB = &Citi;
 
-     pthread_t my_thread;
-       pthread_create(&my_thread,NULL, &thread_atm,(void *) &atm_args );
+     pthread_t my_thread; // ATM thread
+    pthread_create(&my_thread,NULL, &thread_atm,(void *) &atm_args );
 
-
-    // here we make a printing thread - works (without locking yet)
+    // Here we make a printing thread - works (without locking yet)
 
     pthread_t print_status;
     pthread_create(&print_status,NULL, &thread_bank_print,(void *) &Citi );   // need to add the periodic waiting and printing
 
-
+    // Here we make a Fee Charging thread - works (without locking yet)
+    pthread_t charge;
+    pthread_create(&charge, NULL, &thread_bank_charge,(void *) &Citi);
 
     // wait for the threads
     if (pthread_join(my_thread,NULL)){
@@ -53,10 +57,19 @@ int main (int argc, char **argv){
         exit(1);
     }
 
+
+
+    Citi.close(); // need to make a little more complicated - this will happen when all the ATM finish working
+
     if (pthread_join(print_status,NULL)){
         perror("Join Printing Fail");
         exit(1);
     }
-*/
+
+    if (pthread_join(charge, NULL)){
+        perror("Join Charging Fee Fail");
+        exit(1);
+    }
+
     return 0;
 }
